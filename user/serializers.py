08a6a9835b2baaf0ebe6f3eaf2026aa44f968a0d
Mohsen Zahmatkesh <msn.zmt81@gmail.com>
2023-12-29 13:22:42 +0330

    getMe api

---
+++ user/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Person
+from trip.models import TripAdmin
+from salary.models import PaySalaryAdmin
+from news.models import NewsAdmin
+
+
+
+class TripAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TripAdmin
+        fields = ('is_trip_admin',)
+
+class PaySalaryAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaySalaryAdmin
+        fields = ('is_paysalary_admin',)
+
+class NewsAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsAdmin
+        fields = ('is_news_admin',)
+
+class UserSerializer(serializers.ModelSerializer):
+    role = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Person
+        fields = ('id', 'personnelNumber', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined', 'role')
+
+    def get_role(self, obj):
+        if hasattr(obj, 'tripadmin') and obj.tripadmin.is_trip_admin:
+            return 'tripadmin'
+        elif hasattr(obj, 'paysalaryadmin') and obj.paysalaryadmin.is_paysalary_admin:
+            return 'paysalaryadmin'
+        elif hasattr(obj, 'newsadmin') and obj.newsadmin.is_news_admin:
+            return 'newsadmin'
+        else:
+            return None
--- user/views.py
+++ user/views.py
@@ -1,6 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+
 
 
 class LogoutView(GenericAPIView):
@@ -9,3 +13,13 @@ class LogoutView(GenericAPIView):
     def post(self, request):
         request.user.auth_token.delete()
         return Response(data={'message': "Bye!"})
+
+
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
