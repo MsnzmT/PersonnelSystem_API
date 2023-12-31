26821c4e2a8eafea5117624064f8fda9398cbca1
Mohsen Zahmatkesh <msn.zmt81@gmail.com>
2023-12-31 16:51:23 +0330

    add profile to user

--- user/models.py
+++ user/models.py
@@ -109,6 +109,7 @@ class Person(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
     email = models.EmailField(_("email address"), blank=True)
+    profile_picture = models.ImageField(null=True, blank=True, upload_to='user_pictures')
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
