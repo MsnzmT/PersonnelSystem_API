b36c070e469b657018d8d08a17978a2f2c8957d1
Mohsen Zahmatkesh <msn.zmt81@gmail.com>
2024-01-19 17:29:07 +0330

    complete pay salary api

--- salary/urls.py
+++ salary/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from .views import SlipView, SlipDetailView, ModifySlipView, PaymentSlipView
 
 
 urlpatterns = [
-
+    path('slip/', SlipView.as_view()),
+    path('slip-details/<str:personnelNumber>/', SlipDetailView.as_view()),
+    path('modify-slip/<str:personnelNumber>/', ModifySlipView.as_view()),
+    path('payment-slip/<str:personnelNumber>/', PaymentSlipView.as_view()),
 ]
