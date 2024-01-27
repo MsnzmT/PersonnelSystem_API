46ce407c3ed7392be71a1aca5e34b1c66eaaba04
Mohsen Zahmatkesh <msn.zmt81@gmail.com>
2024-01-27 14:04:36 +0330

    trip api's

---
+++ trip/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Trip
+from employee.serializers import EmployeeSerializer
+
+
+class RequestTripSerializer(serializers.ModelSerializer):
+    
+    datetime = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S.000Z", input_formats=["%Y-%m-%dT%H:%M:%S.000Z"])
+    employee_detail = EmployeeSerializer(source='employee', read_only=True)
+    
+    class Meta:
+        model = Trip
+        fields = ('id', 'source', 'destination', 'type', 'datetime', 'seat', 'employee_detail')
+    
+    def create(self, validated_data):
+        user = self.context['request'].user.employee
+        return Trip.objects.create(
+            employee=user,
+            source=validated_data['source'],
+            destination=validated_data['destination'],
+            type=validated_data['type'],
+            seat=int(validated_data['seat']),
+            datetime=validated_data['datetime'],
+        )
